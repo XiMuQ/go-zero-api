34c887f77839fb3c7525be2daa0e9f1639563bd4
“ximuqi” <ximuqi@163.com>
2023-06-28 16:12:55 +0800

    feat：响应信息的统一处理

---
+++ common/response/response.go
@@ -0,0 +1,28 @@
+package response
+
+import (
+	"context"
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"net/http"
+)
+
+type Body struct {
+	Code    int         `json:"code"`
+	Msg     string      `json:"msg"`
+	Success bool        `json:"success"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func Response(ctx context.Context, w http.ResponseWriter, resp interface{}, err error) {
+	var body Body
+	if err != nil {
+		body.Code = 0
+		body.Msg = err.Error()
+	} else {
+		body.Code = 200
+		body.Msg = "success"
+		body.Success = true
+		body.Data = resp
+	}
+	httpx.OkJsonCtx(ctx, w, body)
+}
